0d53fbe33ba989703d653eef8b10c806dbb45b4b
Christopher Tran <christopher.tran@me.com>
2015-08-21 18:08:10 -0700

    app properly read for firebase

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -1,8 +1,11 @@
 'use strict';
 
-var App = angular.module('myApp.dashboard', ['ngRoute', 'firebase'])
+var App = angular.module('myApp.dashboard', [
+	'ngRoute', 'firebase', 'mm.foundation', 'ngAnimate', 'angularSpinner'])
 
-var ref = new Firebase("https://ecom-angularfire.firebaseio.com");
+
+App.constant('FIREBASE_URI', "https://ecom-angularfire.firebaseio.com/");
+
 
 // Declared route 
 App.config(['$routeProvider', function($routeProvider) {
@@ -13,7 +16,55 @@ App.config(['$routeProvider', function($routeProvider) {
 }])
 
 // Register controller
-App.controller('DashboardCtrl', ['$scope', function($scope) {
+App.controller('DashboardCtrl', [
+	'$scope', 'ItemsService', '$modal',
+	function($scope) {
+
+	// $scope.newItem = { price: 0, title: '' };
+	// $scope.currentItem = null;
+
+	// $scope.items = ItemsService.getItems();
+
+	// $scope.addItem = function() {
+	// 	ItemsService.addItem(angular.copy($scope.newItem));
+	// 	$scope.newItem = { price: 0, title: '' };
+	// };
+
+	// $scope.updateItem = function() {
+	// 	ItemsService.updateItem(item);
+	// };
+
+	// $scope.removeItem = function() {
+	// 	ItemsService.removeItem(item);
+	// };
+}]);
+
+App.factory('ItemsService', ['$firebase', 'FIREBASE_URI', function($firebase, FIREBASE_URI) {
+	// var ref = new Firebase(FIREBASE_URI);
+	// var items = $firebase(ref);
 
 
+
+	// var getItems = function() {
+	// 	return items;
+	// };
+
+	// var addItem = function(item) {
+	// 	items.push(item);
+	// };
+
+	// var updateItem = function (item) {
+
+	// };
+
+	// var removeItem = function(item) {
+	// 	items.remove(function(i) {
+	// 		return i['id'] == item.id;
+	// 	});
+	// };
+	// return {
+	// 	getItems: getItems,
+	// 	addItem: addItem,
+	// 	removeItem: removeItem
+	// };
 }]);
--- app/home/home.js
+++ app/home/home.js
@@ -1,7 +1,10 @@
 'use strict';
 
 // *inject firebase.js module from cdn
-var App = angular.module('myApp.home', ['ngRoute', 'firebase', 'angularPayments'])
+var App = angular.module('myApp.home', [
+  'ngRoute', 'firebase', 'angularPayments',
+  'mm.foundation', 'ngAnimate', 'angularSpinner'
+  ])
 
 var ref = new Firebase('https://ecom-angularfire.firebaseio.com/');
 
@@ -16,8 +19,30 @@ App.config(['$routeProvider', function($routeProvider, $window) {
 
 // ====> Home controller
 App.controller('HomeCtrl', [
-   '$scope',
-    function($scope, $http, stripe) {
+   '$scope', '$firebaseObject',
+    function($scope, $firebaseObject, $modal, stripe) {
+
+
+      
+
+      // cart array
+      $scope.cart = [];
+      // binding products array in firebase
+      $scope.products = $firebaseObject(ref.child('products'));
+
+
+      console.log($scope.products);
+
+      // checkout modal
+      $scope.checkout = function() {
+        $modal.open({
+          templateUrl: 'checkout.html',
+          controller: 'CheckoutCtrl'
+        })
+      }
+
+
+
       // Stripe Response Handler
   		$scope.stripeCallback = function(status, res) {
   		    if(res.error) {
